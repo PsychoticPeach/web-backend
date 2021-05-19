b86faa8684182a06a08012f28af537a3c7164f12
PsychoticPeach <miguel.gois2@gmail.com>
2021-05-19 03:06:04 +0100

    erros quase todos feitos.

--- Projeto1/app.js
+++ Projeto1/app.js
@@ -59,7 +59,7 @@ const Product = sequelize.define("Product", {
 //     { seller_id: 1, title: "Jacket", description: "Nice and warm", price: 37, url: "", views: 20948, comments: { "person": "Best Jacket" } },
 //     { seller_id: 1, title: "Chair", description: "Lumbar Support, Confortable", price: 127, url: "", views: 1790, comments: { "person5": "So nice and confy" } },
 //     { seller_id: 2, title: "Computer", description: "ChromeBook", price: 569, url: "", views: 9203, comments: { "Person2": "Love it!" }, tags: ["tags", "Technology"] },
-//     { seller_id: 2, title: "KeyBoard", description: "Razer Huntsman V2", price: 250, url: "", views: 7458, comments: { "Person6": "Can't wait to get mine!" }, tags: {tags:["benfica", "Technology"]} },
+//     { seller_id: 2, title: "KeyBoard", description: "Razer Huntsman V2", price: 250, url: "", views: 7458, comments: { "Person6": "Can't wait to get mine!" }, tags: { tags: ["benfica", "Technology"] } },
 //     { seller_id: 2, title: "SmartBand", description: "Xiaomi miband 6", price: 40, url: "", views: 2042, comments: { "Person4": "Good price for a good band" }, tags: ["porto", "Technology"] },
 // ])
 
@@ -87,77 +87,118 @@ const port = 3000;
 //middleware function
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/project-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //PARTE A
-//Ex a. FEITO
-// app.get("/product", (req, res) => {
-//     Product.findAll().then(product => {
-//         console.log("All Products:", JSON.stringify(product));
-//         res.send(product);
-//     });
-// });
-
-
-//Ex b. FEITO
-// app.post("/product", (req, res) => {
-//     var details = req.body;
-//     Product.create(details).then((product) => {
-//         console.log("Auto generated ID:", product.id);
-//         res.send(JSON.stringify(product.id));
-//     });
-// });
-
-
-//Ex c. FEITO
-app.get("/seller", (req, res) => {
-    var id = req.query.seller_id
-    Product.findAll({
-        where: {
-            id: id
+//Ex a. FEITO INCLUINDO ERROS
+app.get("/product", (req, res) => {
+    Product.findAll().then(product => {
+        if (product.length == 0) {
+            res.status(404);
+            res.end("ID not found!")
+        } else {
+            console.log("200", product);
+            res.status("200").send(product);
         }
-    }).then(product => {
-        console.log("Products from ID:", JSON.stringify(product, null, 4))
-        res.send(JSON.stringify(product, null, 4));
+    }).catch(error => {
+        res.status(404);
+        res.end(error.message);
     });
 });
 
 
-//Ex d. FEITO
+//Ex b. FEITO INCLUINDO ERROS
+app.post("/product", (req, res) => {
+    var details = req.body;
+    Product.create(details).then((product) => {
+        if (details == null) {
+            res.status(404);
+            res.end("ID not found!")
+        } else {
+            console.log("Auto generated ID:", product.id);
+            res.sendStatus(200);
+        }
+    }).catch(error => {
+        res.send({ "error": error });
+    });
+
+});
+
+
+//Ex c. FEITO INCLUINDO ERROS
+app.get("/seller", (req, res) => {
+    Product.findAll({
+        where: {
+            seller_id: req.query.seller_id
+        }
+    }).then(product => {
+        if (product) {
+            res.status(404);
+            res.end("ID not found");
+        } else {
+            console.log("Products from seller_id:", product)
+            res.status(200).send(product);
+        }
+    }).catch(error => {
+        if (req.query.seller_id == undefined) {
+            res.status(400);
+            res.end("ID not Inserted");
+        } else {
+            response.end(error.message);
+        }
+    });
+});
+
+
+//Ex d. FEITO INCLUINDO ERROS
 app.put("/product/:id/incrementViews", (req, res) => {
     Product.findOne({
         where: {
             id: req.params.id
         }
-    })
-    .then(product => {
-        product.increment("views");
-        product.reload();        
-        res.send({"views": product.views});
-    }).catch(error =>{
-        res.send({"error": error});
+    }).then(product => {
+        if (!product) {
+            res.status(404);
+            res.end("ID not found");
+        } else {
+            product.increment("views");
+            product.reload();
+            res.send({ "views": product.views });
+
+        }
+    }).catch(error => {
+        res.send({ "error": error });
     });
 });
 
 
-//Ex e. COMO PESQUISAR POR TAGS  http://localhost:3000/product/tags?tags[]=Technology
+//Ex e. POR FAZER  http://localhost:3000/product/tags?tags[]=Technology FALTA ERROS
 app.get("/product/tags", (req, res) => {
     var tagsArray = req.query.tags;
+
     Product.findAll({}).then(products => {
         for (let i = 0; i < tagsArray.length; i++) {
-            console.log(products[i]);
+            console.log(tagsArray, tagsArray[i]);
+            if (tagsArray[i] == products.tags) {
+                console.log("Estas aqui")
+
+            } else {
+                console.log("Estas aqui no else")
+
+            }
         }
-        console.log("Products with same tags:", product);
-        var tags = JSON.parse(product.tags);
-        res.send(product);
-    }).catch(error =>{
-        res.send({"error": error});
+
+        // console.log("Products with same tags:", products);
+        // var tags = JSON.parse(products.tags);
+        // res.send(products);
+    }).catch(error => {
+        res.send({ "error": error });
     });
 });
 
 
 //PARTE B
-//Ex a. FEITO
+//Ex a. FEITO INCLUINDO ERROS
 app.get("/product/id", (req, res) => {
     var id = req.query.id
     Product.findOne({
@@ -165,8 +206,16 @@ app.get("/product/id", (req, res) => {
             id: id
         }
     }).then(product => {
-        console.log("All Products:", JSON.stringify(product));
-        res.send(product);
+        if (product) {
+            console.log("All Products:", product);
+            res.status(200).send(product);
+        } else {
+            res.status(404);
+            res.end("ID not found!");
+            console.log("ID not found!");
+        }
+    }).catch(error => {
+        res.send({ "error": error });
     });
 });
 
@@ -182,7 +231,7 @@ app.delete("/product/:title", (req, res) => {
         product.destroy(product);
         console.log("200", product);
         res.send("200", product);
-    }).catch(error =>{
+    }).catch(error => {
         res.status(404);
         res.end(error.message);
     });
@@ -191,15 +240,15 @@ app.delete("/product/:title", (req, res) => {
 //Ex c. DONE
 app.put("/product/:id/images", (req, res) => {
     var imagem = req.body;
-    Product.update(imagem,{
+    Product.update(imagem, {
         where: {
             id: req.params.id
         }
     }).then(() => {
         console.log(imagem)
         res.send(imagem);
-    }).catch(error =>{
-        res.send({"error": error});
+    }).catch(error => {
+        res.send({ "error": error });
     });
 });
 
@@ -212,10 +261,10 @@ app.put("/product/:id/comments", (req, res) => {
             id: req.params.id
         }
     }).then((comment) => {
-        console.log("Updated.", "id:", id, comment)
-        res.send(JSON.stringify(comment));
-    }).catch(error =>{
-        res.send({"error": error});
+        console.log(comment)
+        res.send(comment);
+    }).catch(error => {
+        res.send({ "error": error });
     });
 });
 
@@ -223,7 +272,9 @@ app.put("/product/:id/comments", (req, res) => {
 //Ex e. DONE
 app.get("/product/views", (req, res) => {
     Product.findAll({
-        order: [["views", "DESC"]]
+        order: [
+            ["views", "DESC"]
+        ]
     }).then(product => {
         console.log("All Products:", JSON.stringify(product));
         res.send(product);
